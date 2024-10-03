d1523eb9feaa1c7fdf256cd1705ec22ef0664e09
djsanchezc <djsanchezc@indracompany.com>
2024-10-03 02:28:56 -0500

    update for deployed

--- app.py
+++ app.py
@@ -1,40 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import os
-from spleeter.separator import Separator
+from app import create_app
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/separar', methods=['POST'])
-def separar_audio():
-    # Obtener archivo de audio y opción desde el frontend
-    file = request.files['file']
-    opcion = request.form['opcion']
-    
-    # Guardar archivo temporalmente
-    input_audio = os.path.join("temp", file.filename)
-    file.save(input_audio)
-    
-    # Crear la carpeta de salida
-    output_path = os.path.join("temp", "output")
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    # Usar Spleeter para separar
-    separator = Separator('spleeter:2stems')
-    separator.separate_to_file(input_audio, output_path)
-
-    # Seleccionar la pista dependiendo de la opción
-    if opcion == 'voces':
-        output_file = os.path.join(output_path, 'vocals.wav')
-    elif opcion == 'instrumental':
-        output_file = os.path.join(output_path, 'accompaniment.wav')
-    else:
-        return jsonify({'error': 'Opción no válida'}), 400
-    
-    # Devolver la ruta del archivo de salida
-    return jsonify({'output_file': output_file})
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
